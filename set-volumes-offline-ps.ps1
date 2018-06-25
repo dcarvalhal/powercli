664f9c9110d56dc685401b39d81838e27a422e37
dcarvalhal <36567989+dcarvalhal@users.noreply.github.com>
2018-06-25 12:25:12 -0300

    .

--- set-volumes-offline-ps.ps1
+++ set-volumes-offline-ps.ps1
@@ -3,4 +3,7 @@ $GroupAddress = "172.18.35.105"
 
 Connect-EqlGroup -GroupName $GroupName -GroupAddress $GroupAddress -Credential (Get-credential)
 
+ForEach 
+
+
 Disconnect-EqlGroup -GroupAddress $GroupAddress
