5d04dd0c0b0216f67449bed0542651f3721d9b41
dcarvalhal <36567989+dcarvalhal@users.noreply.github.com>
2018-06-25 12:50:32 -0300

    .

--- set-volumes-offline-ps.ps1
+++ set-volumes-offline-ps.ps1
@@ -1,9 +1,25 @@
 $GroupName = "WSB4PRODSTG"
 $GroupAddress = "172.18.35.105"
 
+$VolNames = @( 
+    "Raw-DC1B4CIPDBP01-Data",
+    "Raw-DC1B4CIPDBP01-Logs",
+    "Raw-DC1B4DB03P01-Data-01",
+    "Raw-DC1B4DB03P01-Data-02",
+    "Raw-DC1B4DB03P01-Data-03",
+    "Raw-DC1B4DB03P01-Data-04",
+    "Raw-DC1B4DB03P01-Logs",
+    "Raw-DC1B4GIMDBP02-Data",
+    "Raw-DC1B4GIMDBP02-Logs",
+    "Raw-DC1B4TESOUDBP01-Data",
+    "Raw-DC1B4TESOUDBP01-Logs",
+    "Raw-SCCHIB4VUM-01-Bkp-01"
+			 );
+
 Connect-EqlGroup -GroupName $GroupName -GroupAddress $GroupAddress -Credential (Get-credential)
 
-ForEach 
-
+Foreach ($VolName in $VolNames) {
+    Set-EqlVolume -VolumeName $VolName -OnlineStatus online
+    } 
 
 Disconnect-EqlGroup -GroupAddress $GroupAddress
