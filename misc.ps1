24603c52c23f7b69b5e4efd2dee65599d70e62bb
dcarvalhal <36567989+dcarvalhal@users.noreply.github.com>
2018-06-29 16:00:28 -0300

    add commands

--- misc.ps1
+++ misc.ps1
@@ -1,11 +1,13 @@
 $vms = get-cluster Red | get-vm | where {$_.Name -like "*spv*"}
 Move-VM -VM $vms -Destination Blue
 
-
 Move-VM myVM0 -Destination (Get-Cluster myDestDRSCluster | Get-VMHost | ?{$_.State -eq "Connected"} | Get-Random) -RunAsync
 
-
-
-$vms = get-vm -Location "Autorizador" | where {$_.Name -like "*01*"}
+$vms = get-vm -Location "Glias" | where {$_.Name -like "*01*"}
 Start-VM $vms -Confirm -RunAsync
 get-vm $vms | Shutdown-VMGuest
+
+$vms = get-cluster Blue | get-vm | Select Name, Folder
+
+
+$vms = get-vm -Location Blue | Where-Object {$_.Folder -like "*Aut*"}
