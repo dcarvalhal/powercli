7425e5bfa569510f8e3bd9c0b2cf8f82457e88ed
dcarvalhal <36567989+dcarvalhal@users.noreply.github.com>
2018-06-29 16:42:06 -0300

    .

--- vm-cluster-count.ps1
+++ vm-cluster-count.ps1
@@ -2,9 +2,9 @@ $vmsblue = get-vm -Location Blue | Select-Object Name, Folder
 #$vmsblue | Sort-Object Folder
 $vmsProduction1 = get-vm -Location ClusterProduction1 | Select-Object Name, Folder
 #$vmsProduction1 | Sort-Object Folder
-write-host "VMs cluster Blue" -ForegroundColor "green"
+write-host "VMs cluster Blue" -ForegroundColor "yellow"
 $vmsblue.count
-write-host "VMs cluster Production1" -ForegroundColor "green"
+write-host "VMs cluster Production1" -ForegroundColor "yellow"
 $vmsProduction1.count
 write-host "################################################" -ForegroundColor "Blue"
 $vmsblue | Sort-Object Folder
