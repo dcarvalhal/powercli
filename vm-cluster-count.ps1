9d5597be9308c0984d9005ae81eec2b69ff339eb
dcarvalhal <36567989+dcarvalhal@users.noreply.github.com>
2018-06-29 16:24:34 -0300

    add color

--- vm-cluster-count.ps1
+++ vm-cluster-count.ps1
@@ -5,8 +5,10 @@ $vmsblue | Sort-Object Folder
 $vmsProduction1 = get-vm -Location Blue | Select-Object Name, Folder
 
 $vmsProduction1 | Sort-Object Folder
-
-write-host "VMs cluster Blue" -ForegroundColor "blue"
+write-host "################" -ForegroundColor "Blue"
+write-host "################" -ForegroundColor "Blue"
+write-host "################" -ForegroundColor "Blue"
+write-host "VMs cluster Blue" -ForegroundColor "Blue"
 $vmsblue.count
-write-host "VMs cluster Production1" -ForegroundColor "blue"
+write-host "VMs cluster Production1" -ForegroundColor "green"
 $vmsProduction1.count
