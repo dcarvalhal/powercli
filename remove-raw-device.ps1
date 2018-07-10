ff6f932f5fd6ea17ceb39d5d35a9cca274d13826
dcarvalhal <36567989+dcarvalhal@users.noreply.github.com>
2018-07-10 11:36:42 -0300

    .

--- remove-raw-device.ps1
+++ remove-raw-device.ps1
@@ -1,6 +1,7 @@
 #Conectar no vCenter Server de Produção
 & 'C:\Program Files (x86)\VMware\Infrastructure\PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1'
-Connect-Viserver 172.17.35.35
+$vicredential = Get-Credential
+Connect-VIServer 172.17.35.35 -Credential $vicredential
 
 $vms = @( 
     "DC1B4GIMDBP02",
