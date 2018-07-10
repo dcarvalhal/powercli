e9c5c7efece3aa18e70a7786f4cfd69cf43294e3
dcarvalhal <36567989+dcarvalhal@users.noreply.github.com>
2018-07-10 13:02:44 -0300

    Check volumes

--- check-volumes-so.ps1
+++ check-volumes-so.ps1
@@ -1,28 +1,20 @@
 # Lista de Máquinas
 $vms = @( 
-    "DC1B4GIMDBP02@buy4sc.local",
-    "DC1B4DB05P01@buy4sc.local",
-    "DC1B4DB03P01@buy4sc.local",
-    "DC1B4TESOUDBP01@buy4sc.local"
+    "DC1B4GIMDBP02.buy4sc.local",
+    "DC1B4DB05P01.buy4sc.local",
+    "DC1B4DB03P01.buy4sc.local",
+    "DC1B4TESOUDBP01.buy4sc.local"
     	 );
 
 # Credencial para Remote session
 
-   $cred = Get-Credential 
+#$cred = Get-Credential 
 
-
-$script = get-disk | where {($_.FriendlyName -like "*EQLOGIC*") -and ($_.OperationalStatus -like "*Offline")}
-
-# Looping de execução do script
+# Looping de execução do comando
 
 Foreach ($vm in $vms){            
-   
-   #Cria uma nova sessão especificando o método de autenticação Credssp      
-   $s = New-PSSession -computerName $vm -credential $cred -Authentication Credssp
-     
+        
    # Executa o comando no computador remoto através da sessão
-   Invoke-Command -Session $s -Scriptblock $script  
-   
-   #Sai da sessão
-   Remove-PSSession $s    
+   Invoke-Command -ComputerName $vm -Scriptblock {get-disk | where {($_.FriendlyName -like "*EQLOGIC*") -and ($_.OperationalStatus -like "*Offline")}}
+       
 }
