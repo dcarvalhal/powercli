db3945629dc7513f310c5ec68ab88f10b966fe77
Daniel Carvalhal <36567989+dcarvalhal@users.noreply.github.com>
2018-08-14 13:25:28 -0300

    Rescan all storage

--- rescan-all-storage.ps1
+++ rescan-all-storage.ps1
@@ -1,20 +1,14 @@
-# vCenter Server - ServerCentral #
-$vcenterServer = "172.17.35.35"
-
-# Conecta no vCenter Server - ServerCentral #
-Connect-VIServer $vcenterServer
- 
 # Busca os hosts ESXI #
 $esxis = get-vmhost | Sort-Object name
  
-# Executa o rescan nas HBAs e o rescan dos vmfs em cada hosts - Com um spleep de 60 segundos entre os hosts #
+# Executa o rescan nas HBAs e o rescan dos vmfs em cada hosts - Com um spleep de 30 segundos entre os hosts #
 foreach ($esxi in $esxis)
 	{
 	write-host "Scan all HBAs on "$esxi
-	get-VMHostStorage -VMHost $esxi -RescanAllHba
-    write-host "Rescan VMFS on "$esxi
-    get-VMHostStorage -VMHost $esxi -RescanVmfs
-    Start-Sleep -s 60
+	Get-ScsiLun -VMHost $esxi -RescanAllHba
+    	write-host "Rescan VMFS on "$esxi
+    	Get-ScsiLun -VMHost $esxi -RescanVmfs
+    	Start-Sleep -s 30
 	}
 # Desconecta do vCenter Server #
 Disconnect-VIServer -confirm:$false
